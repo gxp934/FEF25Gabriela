5dcf1f27b8b78a3924a326b9f059a3377fa6d685
gxp934 <gxp934@miami.edu>
2025-09-18 15:28:45 +0000

    test

---
+++ ej/js/debug.js
@@ -0,0 +1,5 @@
+"use strict"; // The "use strict" directive in JavaScript enables strict mode, which enforces stricter parsing and error handling on the code at runtime. It was introduced in ECMAScript 5 to help developers write cleaner, more secure, and less error-prone code. https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Strict_mode
+
+let message = "hello world";
+
+message = "goodbye";
